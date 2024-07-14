defb1cf4beedae462387e25f4b7e1ae71f8422cf
rizalyoga <ach.rizal.yogaswara@gmail.com>
2024-07-14 22:12:48 +0700

    refactor:encryp pass when register & login

--- src/data/studentLogin.ts
+++ src/data/studentLogin.ts
@@ -1,11 +1,16 @@
 import { LoginProps } from "../types/types";
+import CryptoJS from "crypto-js";
 
 const url = process.env.NEXT_PUBLIC_APP_LINK_API;
 
 export const getDataStudent = async (payload: LoginProps) => {
+  const dataLogin = {
+    username: payload.username,
+    password: CryptoJS.SHA256(payload.password).toString(CryptoJS.enc.Hex),
+  };
   const response = await fetch(`${url}?action=getDataStudentByIdLogin`, {
     method: "POST",
-    body: JSON.stringify(payload),
+    body: JSON.stringify(dataLogin),
   });
 
   const data = await response.json();
